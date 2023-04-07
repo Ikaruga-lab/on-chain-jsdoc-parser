cfaaf08e96a0c970ab626fe9ff5f75bf27611239
kyotaw <httg1326@gmail.com>
2023-04-07 20:18:28 +0900

    update addresses

--- scripts/addresses.ts
+++ scripts/addresses.ts
@@ -2,11 +2,11 @@
 export const addresses: {[key:string]: {[key:string]:{[key:string]:string}}} = {
   localhost: {
     parser: {
-      Log: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
-      MeasureGas: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
-      Utf8: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
-      JsDocParser: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
-      JsDocParserProxy: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
+      Utf8: '0x9E545E3C0baAB3E08CdfD552C960A1050f373042',
+  MeasureGas: '0xa82fF9aFd8f496c3d6ac40E2a0F282E47488CFc9',
+  Log: '0x1613beB3B2C4f22Ee086B2b38C1476A3cE7f78E8',
+  JsDocParser: '0x851356ae760d987E095750cCeb3bC6014560891C',
+  JsDocParserProxy: '0xf5059a5D33d5853360D16C683c16e67980206f36',
     }
   },
   goerli: {
